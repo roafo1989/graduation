88b69434a52909ef6700162183a6a3b000ff6158
roafo <roafo@mail.ru>
2020-01-14 05:11:10 +0300

    fix loginForm in SecurityConfig

--- src/main/java/com/example/grad1/controller/security/SecurityConfig.java
+++ src/main/java/com/example/grad1/controller/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -47,10 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/**/**/register").anonymous()
                     .antMatchers("/**").authenticated()
                     .and()
-                .formLogin()
+                .httpBasic(Customizer.withDefaults());
+                /*.formLogin()
                     .permitAll()
                     .and()
                 .logout()
-                    .permitAll();
+                    .permitAll();*/
     }
 }
